2e04ad4121d29332aa30aabf2c4a4e16517db817
= <=>
2024-05-19 21:33:43 +0300

    added menu

--- app/src/main/java/com/perfomax/flexstats/di/AppComponent.kt
+++ app/src/main/java/com/perfomax/flexstats/di/AppComponent.kt
@@ -2,6 +2,7 @@ package com.perfomax.flexstats.di
 
 import com.perfomax.flexstats.accounts.di.AccountsFeatureDeps
 import com.perfomax.flexstats.home.di.HomeFeatureDeps
+import com.perfomax.flexstats.presentation.main.MainActivity
 import com.perfomax.flexstats.presentation.navigation.NavigatorFragment
 import dagger.Component
 import javax.inject.Singleton
--- app/src/main/java/com/perfomax/flexstats/presentation/navigation/NavigatorFragment.kt
+++ app/src/main/java/com/perfomax/flexstats/presentation/navigation/NavigatorFragment.kt
@@ -2,8 +2,17 @@ package com.perfomax.flexstats.presentation.navigation
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.perfomax.flexstats.R
@@ -16,8 +25,6 @@ import javax.inject.Inject
 
 class NavigatorFragment : Fragment(R.layout.fragment_navigator), NavigatorHolder {
 
-    private lateinit var binding: FragmentNavigatorBinding
-
     @Inject
     lateinit var navigatorLifecycle: NavigatorLifecycle
 
@@ -27,9 +34,11 @@ class NavigatorFragment : Fragment(R.layout.fragment_navigator), NavigatorHolder
     @Inject
     lateinit var homeFeatureApi: HomeFeatureApi
 
+    @Inject
+    lateinit var accountsFeatureApi: AccountsFeatureApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentNavigatorBinding.inflate(layoutInflater)
         DaggerProvider.appComponent.inject(this)
         navigatorLifecycle.onCreate(this)
         router.navigateTo(fragment = homeFeatureApi.open())
@@ -37,6 +46,19 @@ class NavigatorFragment : Fragment(R.layout.fragment_navigator), NavigatorHolder
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.nav_menu, menu)
+            }
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when(menuItem.itemId){
+                    R.id.nav_home -> router.navigateTo(fragment = homeFeatureApi.open())
+                    R.id.nav_accounts -> router.navigateTo(fragment = accountsFeatureApi.open())
+                }
+                return true
+            }
+        }, viewLifecycleOwner)
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
